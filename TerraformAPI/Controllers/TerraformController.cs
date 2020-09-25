33aea61cdf94369986800b0ef0076cb6080bc994
raju <devopspractice141829@gmail.com>
2020-09-25 09:53:59 +0530

    commiting new code

---
+++ TerraformAPI/Controllers/TerraformController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AWS;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TerraformAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TerraformController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly ", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public TerraformController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get(string paramsObject)
+        {
+            var data = paramsObject;
+            return "";
+        }
+
+        //[HttpGet]
+        //public string dsdset1(int providerId, string[] resourcenames)
+        //{
+        //    try
+        //    {
+        //        AWS.Resources resources1 = new Resources();
+        //        var resourcescript = resources1.GetScript(providerId, resourcenames);
+        //        StringBuilder resourceScripts = new StringBuilder();
+        //        resourceScripts.Append(resourcescript);
+               
+        //        return resourceScripts.ToString();
+        //    }
+        //    catch (Exception Ex)
+        //    {
+
+        //        return Ex.Message;
+        //    }
+
+        //}
+    }
+}
---
+++ TerraformAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+using AWS;
+
+namespace TerraformAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly ", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController()
+        {
+            
+        }
+        public string GetScript(int providerId, string[] resourcenames)
+        {
+            try
+            {
+                StringBuilder resourceScripts = new StringBuilder();
+                LoadFile();
+                var resources = GetResourceScript();
+
+                if (providerId == 1)
+                {
+
+                    foreach (AWSResources resource in resources)
+                    {
+                        if (resourcenames[0].ToString() == resource.resourceName)
+                        {
+                            resourceScripts.Append(resource.resourceScript);
+                        }
+
+
+                    }
+                }
+
+                return resourceScripts.ToString();
+            }
+            catch (Exception Ex)
+            {
+
+                return Ex.Message;
+            }
+            //Process JSOn File
+            //if (enumAWSReources.re)
+        }
+        private List<AWSResources> GetResourceScript()
+        {
+            try
+            {
+
+                using (StreamReader r = new StreamReader("awsresource.json"))
+                {
+                    string json = r.ReadToEnd();
+                    List<AWSResources> resources = JsonConvert.DeserializeObject<List<AWSResources>>(json);
+                    return resources;
+                }
+
+            }
+            catch (Exception Ex)
+            {
+                string error = Ex.Message;
+                return null;
+            }
+        }
+        private void LoadFile()
+        {
+            try
+            {
+
+                using (StreamReader r = new StreamReader("awsresource.json"))
+                {
+                    string json = r.ReadToEnd();
+                    List<AWSResources> resources = JsonConvert.DeserializeObject<List<AWSResources>>(json);
+                }
+
+            }
+            catch (Exception Ex)
+            {
+                string error = Ex.Message;
+            }
+        }
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
